89e7f66315ab045046fa3da2e9a2144ba2607bf4
niufei1996 <932856678@qq.com>
2018-03-22 11:32:37 +0800

    还原url

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^book',include('book.url')),
-    url('^fly',include('fly.urls')),
+    
 
 ]
